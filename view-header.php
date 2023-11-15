d00b6735239cb24df72913782859db8dd58b98cd
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 02:15:43 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -12,8 +12,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.0"></script>
     <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.0/paper-core.min.js"></script>    
-    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>    
+  
   </head>
   <body>
     
@@ -32,7 +32,7 @@
         <a class="nav-link" href="alpine.php">Alpine.js</a>
         <a class="nav-link" href="anime.php">Anime.js</a>
         <a class="nav-link" href="lodash.php">Lodash</a>
-        <a class="nav-link" href="paper.php">Paper.js</a>        
+        <a class="nav-link" href="jquery.php">jQuery</a>        
 
       </div>
     </div>
