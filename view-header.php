0a620208c007ac24c312b7adc6ee4989bec93bba
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 02:11:02 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -12,6 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.0"></script>
     <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.0/paper-core.min.js"></script>    
     
   </head>
   <body>
