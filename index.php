216c8afe78472edd6bd13718f3f96e99ae8d0b51
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 00:19:04 -0600

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+<h1>Home</h1>
+<?php
+include "view-footer.php";
+?>
+
 <div id="calculator">
     <input type="text" id="display" disabled>
     <table>
