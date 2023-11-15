6f9676dcac4f9fa348632048f508edce02ed09ac
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 00:51:31 -0600

    Update alpine.php

--- alpine.php
+++ alpine.php
@@ -2,6 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
 <body x-data="{ messages: <?php echo json_encode(['Hello', 'Bonjour', 'Hola', 'Ciao']); ?>, currentIndex: 0 }">
 
     <h1 x-text="messages[currentIndex]"></h1>
