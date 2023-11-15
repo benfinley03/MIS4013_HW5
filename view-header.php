285cf3f02c292aa33bc97d67c585084ff480fe47
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 01:23:32 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21"></script>
     
   </head>
   <body>
@@ -29,7 +30,7 @@
         <a class="nav-link" href="calculator.php">Calculator</a>
         <a class="nav-link" href="alpine.php">Alpine.js</a>
         <a class="nav-link" href="anime.php">Anime.js</a>
-
+        <a class="nav-link" href="lodash.php">Lodash</a>
 
       </div>
     </div>
