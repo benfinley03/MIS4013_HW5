a9f7314087437bb1f5a73dbb529fd4d9881a8338
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 01:09:28 -0600

    Update anime.php

--- anime.php
+++ anime.php
@@ -13,10 +13,13 @@ include "view-header.php";
             targets: '#animated-square',
             translateX: 250, // Move 250px to the right
             translateY: 100, // Move 100px down
+            rotate: '1turn', // Rotate one full turn (360 degrees)
+            scale: 2, // Scale the element by a factor of 2
             backgroundColor: '#e74c3c', // Change background color
             duration: 2000, // Animation duration in milliseconds
             easing: 'easeInOutQuad', // Easing function
-            loop: true // Repeat the animation
+            delay: 500, // Delay before starting the animation (in milliseconds)
+            loop: true
         });
     </script>
 
