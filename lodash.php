343b8b45cad93a8e3e9b73277e4fe43add9f1d77
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 02:07:46 -0600

    Update lodash.php

--- lodash.php
+++ lodash.php
@@ -5,7 +5,7 @@ include "view-header.php";
 
 <h1>Simple Page with Lodash</h1>
 
-<button onclick="displayNumbers()">Generate Numbers</button>
+<button onclick="getAndDisplay()">Generate Numbers</button>
     <div id="output"></div>
 
     <script>
@@ -13,23 +13,18 @@ include "view-header.php";
             return Array.from({ length: count }, () => Math.floor(Math.random() * 100) + 1);
         }
 
-        function getNumbers() {
+        function getAndDisplay() {
             const numbers = generateRandomNumbers(5);
             const cubedNumbers = _.map(numbers, n => n * n * n);
             const evenNumbers = _.filter(numbers, n => n % 2 === 0);
 
-            // Return the values
-            return { numbers, cubedNumbers, evenNumbers };
-        }
-
-        function displayNumbers() {
-            const { numbers, cubedNumbers, evenNumbers } = getNumbers();
             document.getElementById('output').innerHTML = `
                 <p>Original Numbers: ${numbers.join(', ')}</p>
                 <p>Cubed Numbers: ${cubedNumbers.join(', ')}</p>
                 <p>Even Numbers from Original Numbers: ${evenNumbers.join(', ')}</p>
             `;
         }
+    </script>
     </script>
 
 <?php
