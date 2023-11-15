4a2d0d548f5c5cf8adfd325ecb0e206c29c7529b
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 01:24:04 -0600

    Create lodash.php

---
+++ lodash.php
@@ -0,0 +1,19 @@
+ <h1>Simple Page with Lodash</h1>
+
+    <div id="output"></div>
+
+    <script>
+        // Sample array of numbers
+        const numbers = [1, 2, 3, 4, 5];
+
+        // Use Lodash functions to manipulate the array
+        const squaredNumbers = _.map(numbers, n => n * n);
+        const evenNumbers = _.filter(numbers, n => n % 2 === 0);
+
+        // Display the results on the page
+        document.getElementById('output').innerHTML = `
+            <p>Original Numbers: ${numbers.join(', ')}</p>
+            <p>Squared Numbers: ${squaredNumbers.join(', ')}</p>
+            <p>Even Numbers: ${evenNumbers.join(', ')}</p>
+        `;
+    </script>
