c0bc4cb75be730c910feb6377ac7c8dd2b87bd57
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 01:42:35 -0600

    Update lodash.php

--- lodash.php
+++ lodash.php
@@ -1,35 +1,34 @@
 <?php
-$pageTitle = "Home";
+$pageTitle = "Lodash";
 include "view-header.php";
 ?>
 
 <h1>Simple Page with Lodash</h1>
 
-    <button onclick="generateRandomNumbers(5)">Generate Numbers</button>
+<button onclick="displayNumbers()">Generate Numbers</button>
     <div id="output"></div>
-
-    
 
     <script>
         function generateRandomNumbers(count) {
             return Array.from({ length: count }, () => Math.floor(Math.random() * 100) + 1);
-            displayNumbers();
         }
 
         function getNumbers() {
-        const numbers = generateRandomNumbers(5); // Change 10 to the desired count
-        const cubedNumbers = _.map(numbers, n => n * n * n);
-        const evenNumbers = _.filter(numbers, n => n % 2 == 0);
+            const numbers = generateRandomNumbers(5);
+            const cubedNumbers = _.map(numbers, n => n * n * n);
+            const evenNumbers = _.filter(numbers, n => n % 2 === 0);
 
+            // Return the values
+            return { numbers, cubedNumbers, evenNumbers };
         }
 
         function displayNumbers() {
-        getNumbers();
-        document.getElementById('output').innerHTML = `
-            <p>Original Numbers: ${numbers.join(', ')}</p>
-            <p>Cubed Numbers: ${cubedNumbers.join(', ')}</p>
-            <p>Even Numbers from Original Numbers: ${evenNumbers.join(', ')}</p>
-        `;
+            const { numbers, cubedNumbers, evenNumbers } = getNumbers();
+            document.getElementById('output').innerHTML = `
+                <p>Original Numbers: ${numbers.join(', ')}</p>
+                <p>Cubed Numbers: ${cubedNumbers.join(', ')}</p>
+                <p>Even Numbers from Original Numbers: ${evenNumbers.join(', ')}</p>
+            `;
         }
     </script>
 
