ff16115481730c42de7a12d75e0c69ffbcd2b268
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 00:35:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,11 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    
+    
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha384-oGN9/X1Ld9oYl/9ek1ojpy1QxElJCTOHf8bY6Q5cwoMau4YKS/ZtK3uFfQGJzI1e3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
   </head>
   <body>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
 <div class = "containers">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
