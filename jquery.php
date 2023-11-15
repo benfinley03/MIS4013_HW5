7dd4eff0190e9dda58c9224aff0eb9f65360b389
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 02:18:06 -0600

    Update jquery.php

--- jquery.php
+++ jquery.php
@@ -8,10 +8,14 @@ include "view-header.php";
     <p id="myParagraph" style="display: none;">This is a hidden paragraph. Click the button to toggle its visibility.</p>
 
     <script>
-        // jQuery code to toggle paragraph visibility
+        // jQuery code with animations and dynamic text
         $(document).ready(function() {
             $("#toggleButton").click(function() {
-                $("#myParagraph").toggle();
+                $("#myParagraph").slideToggle(500, function() {
+                    // Animation complete callback
+                    const buttonText = $(this).is(":visible") ? "Hide Paragraph" : "Show Paragraph";
+                    $("#toggleButton").text(buttonText);
+                });
             });
         });
     </script>
