44bb5de631526b574cb9c8095fd79ed11f96372a
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 00:50:42 -0600

    Update alpine.php

--- alpine.php
+++ alpine.php
@@ -2,16 +2,18 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<body x-data="{ messages: ['Hello', 'Bonjour', 'Hola', 'Ciao'], currentIndex: 0 }">
+<body x-data="{ messages: <?php echo json_encode(['Hello', 'Bonjour', 'Hola', 'Ciao']); ?>, currentIndex: 0 }">
 
     <h1 x-text="messages[currentIndex]"></h1>
 
     <button @click="changeMessage">Change Message</button>
 
     <script>
+        // Alpine.js data initialization
         document.addEventListener('alpine:init', () => {
             Alpine.data('messages', () => ({
                 currentIndex: 0,
+                messages: <?php echo json_encode(['Hello', 'Bonjour', 'Hola', 'Ciao']); ?>,
                 changeMessage() {
                     this.currentIndex = (this.currentIndex + 1) % this.messages.length;
                 },
