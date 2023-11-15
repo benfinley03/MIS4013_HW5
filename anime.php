b84901421dbee5d6759222a67543c8a9d8d88a3f
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-11-15 01:07:14 -0600

    Create anime.php

---
+++ anime.php
@@ -0,0 +1,14 @@
+<div id="animated-square" style="width: 100px; height: 100px; background-color: #3498db;"></div>
+
+    <script>
+        // Use Anime.js to create animations
+        anime({
+            targets: '#animated-square',
+            translateX: 250, // Move 250px to the right
+            translateY: 100, // Move 100px down
+            backgroundColor: '#e74c3c', // Change background color
+            duration: 2000, // Animation duration in milliseconds
+            easing: 'easeInOutQuad', // Easing function
+            loop: true // Repeat the animation
+        });
+    </script>
